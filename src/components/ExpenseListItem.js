a3519774f0c9bfab17f996dcdaf6c3ee94e7c9a5
Hartjenstein <hartjenstein@gmail.com>
2018-09-01 11:27:52 +0200

    udpated package.json

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
  
 const ExpenseListItem = ({ id, description, amount, createdAt, note  } ) => (
     <div className="ExpenseListItem">
         {description &&  <Link to={`/edit/${id}`}><h3>{description}</h3></Link>}
         {note &&  <p>Note: {note}</p>}
         {amount &&  <p>Amount: {amount}</p>}
-        {createdAt &&  <p>Time created: {createdAt}</p>}
+        {createdAt &&  <p>Time created: {moment(createdAt).format('MM Do, YYYY')}</p>}
     </div>
 )
 
