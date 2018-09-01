8451d3c4889b4183e889e0548fe834f2fa0a9482
Hartjenstein <hartjenstein@gmail.com>
2018-09-01 17:45:05 +0200

    bugfix typo in import

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/Approuter';
+import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses.js';
 import getVisibleExpenses from './selectors/expenses.js';
